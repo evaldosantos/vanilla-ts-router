455a9e2b46d85e102e702c6348089ed7bb672e42
Evaldo Santos <jesantosjr@protonmail.com>
2020-04-11 21:10:24 +0200

    handle link clicks

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("welcome to parcel base project! Have fun!")
+window.onload = () => {
+  const routeElements: NodeListOf<HTMLElement> = document.querySelectorAll('.route');
+
+  // Add history push() event when boxes are clicked
+  routeElements.forEach(routeElement => routeElement.addEventListener('click', pushEvent));
+}
+
+function pushEvent(event) {  
+  console.log(event);
+}
