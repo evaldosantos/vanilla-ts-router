e1fc6f8562584eeda7c54262ca8bc2fdf5329b5d
Evaldo Santos <jesantosjr@protonmail.com>
2020-04-11 21:19:21 +0200

    implementing handling event

--- index.ts
+++ index.ts
@@ -5,6 +5,24 @@ window.onload = () => {
   routeElements.forEach(routeElement => routeElement.addEventListener('click', pushEvent));
 }
 
-function pushEvent(event) {  
-  console.log(event);
+function pushEvent(event: MouseEvent): void {
+  const target = event.target as HTMLElement;
+  // Get id attribute of the box or button or link clicked
+  let id = target.id;
+  // Visually select the clicked button/tab/box
+  selectTab(id);
+  // Update Title in Window's Tab
+  document.title = id;
+  // Load content for this tab/page
+  loadContent(id);
+  // Finally push state change to the address bar
+  window.history.pushState({id}, `${id}`, `/page/${id}`);
+}
+
+function selectTab(id: string): void {
+  console.log("select tab", id);
+}
+
+function loadContent(id: string) {
+  console.log("loading content...", id);
 }
